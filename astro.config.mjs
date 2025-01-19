487cd86dad3e050efee76bdb025f73c369af7745
vvaz <valerio@wpbox.io>
2025-01-19 18:28:09 +0000

    add tailwind e levantamentos eupago

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,34 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
+
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Tipme',
-					items: [
-						{ label: 'Sobre', slug: 'about/us' },
-					],
-				},
-				{
-					label: 'Métodos de Pagamento',
-					items: [
-						{ label: 'Intro', slug: 'payment-methods/intro' },
-						{ label: 'MBWay', slug: 'payment-methods/mbway' },
-					],
-				},
+  integrations: [
+    starlight({
+      title: 'Docs',
+      customCss: [
+        // Path to your Tailwind base styles:
+        './src/tailwind.css',
+      ],
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Tipme',
+          items: [
+            { label: 'Sobre', slug: 'about/us' },
+          ],
+        },
+        {
+          label: 'Métodos de Pagamento',
+          items: [
+            { label: 'Intro', slug: 'payment-methods/intro' },
+            { label: 'MBWay', slug: 'payment-methods/mbway' },
+          ],
+        },
         {
           label: 'Instituições de Pagamento',
           items: [
@@ -34,15 +39,16 @@ export default defineConfig({
                 { label: 'Registo na Eupago', slug: 'payment-gateways/eupago/registo-eupago' },
                 { label: 'Tempo de resposta da Eupago', slug: 'payment-gateways/eupago/eupago-tempo-resposta' },
                 { label: 'Documentos necessários', slug: 'payment-gateways/eupago/documentos' },
+                { label: 'Levantamento de fundos', slug: 'payment-gateways/eupago/levantamento' },
               ],
-            }
+            },
           ],
         },
-				/* {
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				}, */
-			],
-		}),
-	],
+      ],
+    }),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
 });
